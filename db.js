99403e17d3308f0feaa7021cb8d603f2e2eb65c8
rober hernandez paternina <roberthernandezpaternina25@gmail.com>
2026-03-26 22:38:40 -0500

    cambios1

--- db.js
+++ db.js
@@ -1,8 +1,28 @@
-const { Pool } = require("pg");
+const mysql = require('mysql2');
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+const connection = mysql.createConnection({
+  // 1. El host es solo la dirección del servidor (sin el mysql:// y sin el puerto)
+  host: 'gondola.proxy.rlwy.net', 
+  
+  // 2. El usuario es 'root' (lo dice al principio de tu cadena)
+  user: 'root',
+  
+  // 3. La contraseña es lo que está después de los dos puntos y antes del @
+  password: 'mKAefsjATacOBXKVZwDKaGGEedXicEKy',
+  
+  // 4. El nombre de la base de datos es lo que está al final después de la barra /
+  database: 'railway', 
+  
+  // 5. El puerto es el número que está después de los dos puntos al final
+  port: 14829 
 });
 
-module.exports = pool;
+connection.connect((err) => {
+  if (err) {
+    console.error('Error conectando a Railway:', err.message);
+    return;
+  }
+  console.log('¡Conectado exitosamente a la base de datos en la nube!');
+});
+
+module.exports = connection;
